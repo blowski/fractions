df9119579ef4cf44bff73f6071aa3b3346d0106b
Dan Blows <d@nblo.ws>
2020-01-03 14:37:22 +0000

    Added test cases for dividing by fractions

--- tests/DivideFractionsTest.php
+++ tests/DivideFractionsTest.php
@@ -27,4 +27,29 @@ class DivideFractionsTest extends TestCase
             'negative divided by negative' => [-8, -4, 2],
         ];
     }
+
+    /**
+     * @test
+     * @dataProvider exampleFractionCases
+     */
+    function it_divides_fractions(string $dividend, string $divisor, string $expectedQuotient): void
+    {
+        $quotient = Fraction::fromString($dividend)->dividedBy(Fraction::fromString($divisor));
+        self::assertEquals(
+            Fraction::fromString($expectedQuotient),
+            $quotient,
+            "Expected {$dividend} - {$divisor} to equal $expectedQuotient, but got $quotient"
+        );
+    }
+
+    public function exampleFractionCases()
+    {
+        return [
+            'zero divided by positive fraction' => ['0', '1/5', '0'],
+            'positive fraction divided by positive fraction' => ['3/4', '5/8', '6/5'],
+            'zero divided by negative fraction' => ['0', '-1/7', '0'],
+            'positive fraction divided by negative fraction' => ['7/9', '-1/4', '-28/9'],
+            'negative fraction divided by negative fraction' => ['-10/13', '-1/5', '50/13'],
+        ];
+    }
 }
